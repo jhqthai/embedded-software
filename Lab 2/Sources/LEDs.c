eace8f309571ea35f732a4edeab7ddb27be8bf44
gav0 <jgav95@gmail.com>
2017-04-03 16:08:03 +1000

    Added lab2 source files
    Also updated types header file

---
+++ Lab 2/Sources/LEDs.c
@@ -0,0 +1,47 @@
+/*! @file
+ *
+ *  @brief Routines to access the LEDs on the TWR-K70F120M.
+ *
+ *  This contains the functions for operating the LEDs.
+ *
+ *  @author PMcL
+ *  @date 2015-08-15
+ */
+
+// new types
+#include "types.h"
+
+/*! @brief Sets up the LEDs before first use.
+ *
+ *  @return bool - TRUE if the LEDs were successfully initialized.
+ */
+bool LEDs_Init(void){
+
+}
+
+/*! @brief Turns an LED on.
+ *
+ *  @param color The color of the LED to turn on.
+ *  @note Assumes that LEDs_Init has been called.
+ */
+void LEDs_On(const TLED color){
+
+}
+
+/*! @brief Turns off an LED.
+ *
+ *  @param color THe color of the LED to turn off.
+ *  @note Assumes that LEDs_Init has been called.
+ */
+void LEDs_Off(const TLED color){
+
+}
+
+/*! @brief Toggles an LED.
+ *
+ *  @param color THe color of the LED to toggle.
+ *  @note Assumes that LEDs_Init has been called.
+ */
+void LEDs_Toggle(const TLED color){
+
+}
